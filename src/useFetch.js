2f756a36a6e58a86aeb71ecb345ae12a25a284e2
Roniell Perez <roniell2016@hotmail.com>
2021-12-24 01:06:59 -0400

    The function was put within the useEffect

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,23 +4,25 @@ export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const getData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data) setData(data.data);
-      else setData([]);
-    } catch (error) {
-      console.log(error);
-    }
-    setLoading(false);
-  };
-
   useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data) setData(data.data);
+        else setData([]);
+      } catch (error) {
+        console.log(error);
+      }
+
+      setLoading(false);
+    };
+
     getData();
   }, [url]);
 
-  return { loading, data };
+  return { data, loading };
 };
