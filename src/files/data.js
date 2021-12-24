f14d7f44e03df3db9dcf6aa2fad923e0b41024e4
Roniell Perez <roniell2016@hotmail.com>
2021-12-24 00:56:31 -0400

    API link and array grades added

--- src/files/data.js
+++ src/files/data.js
@@ -1,7 +1,13 @@
 export const author = "Roniell Pérez Bello";
+
 export const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+export const grades = ["A", "B", "C", "F"];
+
 export const listItems = [
   { name: "inicio", path: "/" },
   { name: "acerca de", path: "/about" },
   { name: "contacto", path: "/contact" },
 ];
+
+export const apiItla = "https://nodejs-itlamaterias.herokuapp.com/carreras";
